b38ba2b73067b7186a2a79cd169dbe56575dc5c2
Jishan-Kapoor <24f1002364@ds.study.iitm.ac.in>
2025-10-16 12:01:52 -0400

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Install Tesseract OCR
-apt-get update && apt-get install -y tesseract-ocr
-
-# Upgrade pip and install dependencies
-pip install --upgrade pip
-pip install -r requirements.txt
